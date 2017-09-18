af125d1a38fcad8b76d4f5bddc56d1432d4be902
lu wang <snow.wang729@gmail.com>
2017-09-17 20:30:59 -0400

    d

--- assess_learners/DTLearner.py
+++ assess_learners/DTLearner.py
@@ -35,30 +35,40 @@ class DTLearner(object):
 
         return left_index, right_index, split_index, split_value
 
-    def build_tree(self, Xtrain, Ytrain):
+    def build_tree(self, x_train, y_train):
 
-        if Xtrain.shape[0] == 0:
+        num_instances = x_train.shape[0]
+        if num_instances == 0:
+            print 'all -1s'
             return np.array([-1, -1, -1, -1])
-        if Xtrain.shape[0] == 1:
-            return np.array([-1, Ytrain[0], None, None])
-        if len(np.unique(Ytrain)) == 1:
-            return np.array([-1, Ytrain[0], None, None])
-        if Xtrain.shape[0] <= self.leaf_size:
-            return np.array([-1, np.mean(Ytrain), -1, -1])
+        if num_instances <= self.leaf_size:
+            # If there's only one instance, take the mean of the labels
+            return np.array([-1, np.mean(y_train), -1, -1])
 
-        # get the left, right indexes, the max index, and the split value
+        values = np.unique(y_train)
+        if len(values) == 1:
+            # If all instances have the same label, return that label
+            return np.array([-1, y_train[0], -1, -1])
 
-        left_index, right_index, split_index, split_value = self.get_indexes(Xtrain, Ytrain)
+        # Choose a random feature, and a random split value
+        left_indices, right_indices, feature_index, split_val = \
+            self.get_split_indices(x_train, num_instances)
 
-        left_Xtrain = np.array([Xtrain[i] for i in left_index])
-        left_Ytrain = np.array([Ytrain[i] for i in left_index])
-        right_Xtrain = np.array([Xtrain[i] for i in right_index])
-        right_Ytrain = np.array([Ytrain[i] for i in right_index])
 
-        left_tree = self.build_tree(left_Xtrain, left_Ytrain)
-        right_tree = self.build_tree(right_Xtrain, right_Ytrain)
-        root = [split_index, split_value, 1, left_tree.shape[0] + 1]
-        return np.append(root, left_tree, right_tree)
+
+        left_x_train = np.array([x_train[i] for i in left_indices])
+        left_y_train = np.array([y_train[i] for i in left_indices])
+        right_x_train = np.array([x_train[i] for i in right_indices])
+        right_y_train = np.array([y_train[i] for i in right_indices])
+
+        left_tree = self.build_tree(left_x_train, left_y_train)
+        right_tree = self.build_tree(right_x_train, right_y_train)
+        if len(left_tree.shape) == 1:
+            num_left_side_instances = 2
+        else:
+            num_left_side_instances = left_tree.shape[0] + 1
+        root = [feature_index, split_val, 1, num_left_side_instances]
+        return np.vstack((root, np.vstack((left_tree, right_tree))))
 
     def traverse_tree(self, instance, row=0):
         feature_index = int(self.tree[row][0])
