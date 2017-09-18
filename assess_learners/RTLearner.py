a5a6cd25ec2581490a452c0988fd8cf31693316e
lu wang <snow.wang729@gmail.com>
2017-09-17 23:29:06 -0400

    m

--- assess_learners/RTLearner.py
+++ assess_learners/RTLearner.py
@@ -35,45 +35,47 @@ class RTLearner(object):
 
             return np.array([-1, np.mean(Ytrain), -1, -1])
 
-        Y_values = np.unique(Ytrain)
-        if len(Y_values) == 1:
+        values = np.unique(Ytrain)
+        if len(values) == 1:
 
             return np.array([-1, Ytrain[0], -1, -1])
 
-        left_index, right_index, index, split_value =  self.get_indexes(Xtrain, Xtrain.shape[0])
+        # Choose a random feature, and a random split value
+        left_indices, right_indices, feature_index, split_val = \
+            self.get_indexes(Xtrain, Xtrain.shape[0])
 
-        while len(left_index) < 1 or len(right_index) < 1:
-            left_index, right_index, index, split_value = \
+        while len(left_indices) < 1 or len(right_indices) < 1:
+            left_indices, right_indices, feature_index, split_val = \
                 self.get_indexes(Xtrain, Xtrain.shape[0])
 
-        left_x_train = np.array([Xtrain[i] for i in left_index])
-        left_y_train = np.array([Ytrain[i] for i in right_index])
-        right_x_train = np.array([Xtrain[i] for i in right_index])
-        right_y_train = np.array([Ytrain[i] for i in right_index])
+        left_x_train = np.array([Xtrain[i] for i in left_indices])
+        left_y_train = np.array([Ytrain[i] for i in left_indices])
+        right_x_train = np.array([Xtrain[i] for i in right_indices])
+        right_y_train = np.array([Ytrain[i] for i in right_indices])
 
         left_tree = self.build_tree(left_x_train, left_y_train)
         right_tree = self.build_tree(right_x_train, right_y_train)
         if len(left_tree.shape) == 1:
-            num_left = 2
+            num_left_side_instances = 2
         else:
-            num_left = left_tree.shape[0] + 1
-        root = [index, split_value, 1, num_left]
+            num_left_side_instances = left_tree.shape[0] + 1
+        root = [feature_index, split_val, 1, num_left_side_instances]
         return np.vstack((root, np.vstack((left_tree, right_tree))))
 
     def addEvidence(self, Xtrain, Ytrain):
         self.tree = self.build_tree(Xtrain, Ytrain)
 
-    def traverse(self, instance, row=0):
-        index = int(self.tree[row][0])
-        if index == -1:
+    def traverse_tree(self, instance, row=0):
+        feature_index = int(self.tree[row][0])
+        if feature_index == -1:
             return self.tree[row][1]
-        if instance[index] <= self.tree[row][1]:
-            return self.traverse(instance, row + int(self.tree[row][2]))
+        if instance[feature_index] <= self.tree[row][1]:
+            return self.traverse_tree(instance, row + int(self.tree[row][2]))
         else:
-            return self.traverse(instance, row + int(self.tree[row][3]))
+            return self.traverse_tree(instance, row + int(self.tree[row][3]))
 
     def query(self, Xtest):
         result = []
         for instance in Xtest:
-            result.append(self.traverse(instance))
+            result.append(self.traverse_tree(instance))
         return np.array(result)
