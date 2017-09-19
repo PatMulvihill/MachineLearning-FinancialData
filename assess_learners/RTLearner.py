f72b6f5dc8d2f7af2a9ec508953dbf8840e2b19b
lu wang <snow.wang729@gmail.com>
2017-09-18 20:12:56 -0400

    m

--- assess_learners/RTLearner.py
+++ assess_learners/RTLearner.py
@@ -11,15 +11,15 @@ class RTLearner(object):
     def author(self):
         return 'lwang496'
 
-    def get_indexes(self, x_train, num_instances):
-        index = randint(0, x_train.shape[1] - 1)
-        index1 = randint(0, num_instances - 1)
-        index2 = randint(0, num_instances - 1)
-        split_value = (x_train[index1][index] + x_train[index2][index]) / 2
+    def get_indexes(self, Xtrain):
+        index = randint(0, Xtrain.shape[1] - 1)
+        index1 = randint(0, Xtrain.shape[0] - 1)
+        index2 = randint(0, Xtrain.shape[0] - 1)
+        split_value = (Xtrain[index1][index] + Xtrain[index2][index]) / 2
         left_index = []
         right_index = []
-        for i in xrange(x_train.shape[0]):
-            if x_train[i][index] <= split_value:
+        for i in xrange(Xtrain.shape[0]):
+            if Xtrain[i][index] <= split_value:
                 left_index.append(i)
             else:
                 right_index.append(i)
@@ -27,7 +27,7 @@ class RTLearner(object):
         return left_index, right_index, index, split_value
 
     def build_tree(self, Xtrain, Ytrain):
-
+        # builder the tree based on the Xtrain and Ytrain
         if Xtrain.shape[0] < 1:
             return np.array([-1, -1, None, None])
 
@@ -40,7 +40,7 @@ class RTLearner(object):
         left_index, right_index, split_index, split_value = [],[],[],[]
 
         while len(left_index) < 1 or len(right_index) < 1:
-            left_index, right_index, split_index, split_value = self.get_indexes(Xtrain, Xtrain.shape[0])
+            left_index, right_index, split_index, split_value = self.get_indexes(Xtrain)
 
         left_Xtrain = np.array([Xtrain[i] for i in left_index])
         right_Xtrain = np.array([Xtrain[i] for i in right_index])
@@ -57,21 +57,11 @@ class RTLearner(object):
         return np.vstack((root, left_tree, right_tree))
 
     def addEvidence(self, Xtrain, Ytrain):
+        # return the tree I built
         self.tree = self.build_tree(Xtrain, Ytrain)
 
-    def traverse(self, each_test, row=0):
-        index = int(self.tree[row][0])
-        if index < 0:
-            return self.tree[row][1]
-        if each_test[index] <= self.tree[row][1]:
-            return self.traverse(each_test, row + int(self.tree[row][2]))
-        else:
-            return self.traverse(each_test, row + int(self.tree[row][3]))
-
-
-
     def query(self, Xtest):
-
+        # traverse the tree, find the leaf
         result = []
         for each_test in Xtest:
             row = 0
