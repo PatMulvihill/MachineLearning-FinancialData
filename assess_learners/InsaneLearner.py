2d098dbf7f571a9618682cf645bfb2d2c47a7e8e
lu wang <snow.wang729@gmail.com>
2017-09-19 10:58:10 -0400

    m

---
+++ assess_learners/InsaneLearner.py
@@ -0,0 +1,46 @@
+import numpy as np
+import BagLearner as Bag
+from random import randint
+
+class InsaneLearner(object):
+
+    def __init__(self, verbose):
+        self.learner = Bag.learner
+        self.kwargs = Bag.kwargs
+        self.bags = Bag.bags
+        self.boost = Bag.boost
+        self.verbose = verbose
+
+    def author(self):
+        return 'lwang496'
+
+    def addEvidence(self,Xtrain,Ytrain):
+        # Randomly select the set of data
+        index = []
+        for i in range(self.bags):
+            index.append(np.random.random_integers(0, Xtrain.shape[0] - 1, Xtrain.shape[0]))
+
+        self.Xbags = []
+        self.Ybags = []
+        for i in range(self.bags):
+            self.Xbags.append(Xtrain[index[i]])
+            self.Ybags.append(Ytrain[index[i]])
+
+    def query(self,Xtest):
+
+        result = []
+        learners = []
+
+        for i in range(0,self.bags):
+            learners.append(self.learner(**self.kwargs))
+
+        for i in range(0,len(learners)):
+            learners[i].addEvidence(self.Xbags[i],self.Ybags[i])
+            result.append(learners[i].query(Xtest))
+
+        if self.verbose:
+            print np.mean(np.array(result), axis=0)
+
+        return np.mean(np.array(result), axis=0)
+
+
