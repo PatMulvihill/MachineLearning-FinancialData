acda640e6b83f45d5562b4554c713175c3a81e7f
lu wang <snow.wang729@gmail.com>
2017-11-25 10:47:46 -0500

    m

--- strategy_learner/StrategyLearner.py
+++ strategy_learner/StrategyLearner.py
@@ -84,9 +84,9 @@ class StrategyLearner(object):
         train_indicator = pd.concat([train_bbp, train_momentum, train_vol],
                                     keys=['Ind1', 'Ind2', 'Ind3'], axis=1)
 
-        train_states = train_bbp[symbol] * 100 + \
-                       train_momentum[symbol] * 10 + \
-                       train_vol[symbol]
+        train_states = train_bbp.ix[:,0] * 100 + \
+                       train_momentum.ix[:,0] * 10 + \
+                       train_vol.ix[:,0]
 
         Qframe = pd.DataFrame(index=pd.date_range(train_states.index[0], train_states.index[-1]),
                               columns=['Pos', 'Price', 'Cash', 'P_V'])
