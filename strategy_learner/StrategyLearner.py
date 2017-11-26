71c248a3f59c231291d21abf23a5d7a10621ecdd
lu wang <snow.wang729@gmail.com>
2017-11-25 22:24:08 -0500

    m

--- strategy_learner/StrategyLearner.py
+++ strategy_learner/StrategyLearner.py
@@ -68,7 +68,7 @@ class StrategyLearner(object):
 
         train_SMAPrice_ratio_n, train_bbp_n, train_momentum_n, train_size_n = self.discritize(train_SMAPrice_ratio, train_bbp, train_momentum, train_size)
 
-        strategy = train_SMAPrice_ratio_n * 5 + train_bbp_n * 100 + train_momentum_n * 5 + train_size_n
+        strategy = train_SMAPrice_ratio_n * 100 + train_bbp_n * 10 + train_momentum_n * 10 + train_size_n
         start = strategy.index[0]
         end = strategy.index[-1]
         dates = pd.date_range(start, end)
@@ -168,29 +168,29 @@ class StrategyLearner(object):
 
         test_SMA_ratio_n, test_bbp_n, test_momentum_n, test_size_n = self.discritize(test_SMAPrice_ratio,test_bbp,test_momentum, test_size)
 
-        test_strategy =test_SMA_ratio_n * 5 + test_bbp_n * 100 + test_momentum_n * 5 + test_size_n
+        test_strategy = test_bbp_n * 100 + test_SMA_ratio_n + test_momentum_n * 5 + test_size_n
         test_strategy_states = test_strategy.values
         p = 0
         test_total_dates = test_strategy_states.size
         for i in range(1, test_total_dates):
-            state = test_strategy_states[ i - 1, 0] + p *1000
+            state = p *900 + test_strategy_states[ i - 1, 0]
             action = self.learner.querysetstate(state)
             if p == 0 and action == 1:
 
-                status = -1000
+                trades.ix[i, symbol] = -1000
                 p = 1
             elif p == 0 and action == 2:
-                status = 1000
+                trades.ix[i, symbol] = 1000
                 p = 2
 
             elif p == 1 and action == 2:
-                status = 2000
+                trades.ix[i, symbol] = 2000
                 p = 2
 
             elif p == 2 and action == 1:
-                status = -2000
+                trades.ix[i, symbol] = -2000
                 p = 1
-            trades.ix[i, symbol] = status
+
         if self.verbose: print type(trades)  # it better be a DataFrame!
         if self.verbose: print trades
         if self.verbose: print prices_all
