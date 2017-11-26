7f43a26cf5a9e53e5c679ab5600ad5e914c7feca
lu wang <snow.wang729@gmail.com>
2017-11-25 22:33:14 -0500

    m

--- strategy_learner/StrategyLearner.py
+++ strategy_learner/StrategyLearner.py
@@ -168,12 +168,12 @@ class StrategyLearner(object):
 
         test_SMA_ratio_n, test_bbp_n, test_momentum_n, test_size_n = self.discritize(test_SMAPrice_ratio,test_bbp,test_momentum, test_size)
 
-        test_strategy = test_bbp_n * 100 + test_momentum_n * 10 + test_size_n
+        test_strategy = test_bbp_n * 100 + test_momentum_n * 10 + test_SMA_ratio_n + test_size_n
         test_strategy_states = test_strategy.values
         p = 0
         test_total_dates = test_strategy_states.size
         for i in range(1, test_total_dates):
-            state = test_strategy_states[ i - 1, 0] + p *1000 
+            state = test_strategy_states[ i - 1, 0] + p *1000
             action = self.learner.querysetstate(state)
             status = 0
             if p == 0 and action == 1:
