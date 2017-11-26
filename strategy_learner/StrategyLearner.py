7e0fb01d55de4dae8aca827394d534ba053bcd05
lu wang <snow.wang729@gmail.com>
2017-11-25 21:02:23 -0500

    m

--- strategy_learner/StrategyLearner.py
+++ strategy_learner/StrategyLearner.py
@@ -169,31 +169,30 @@ class StrategyLearner(object):
 
         test_SMA_ratio_n, test_bbp_n, test_momentum_n, test_vol_n = self.discritize(test_SMAPrice_ratio,test_bbp,test_momentum, test_size)
 
+        test_states = test_bbp_n * 50 + test_SMA_ratio_n * 50 + test_momentum_n * 10 + test_vol_n
 
-        test_strategy = test_bbp_n * 50 + test_SMA_ratio_n * 50 + test_momentum_n * 10 + test_vol_n
+        test_states = test_states.values
+        position = 0
 
-        test_strategy_states = test_strategy.values
-        p = 0
-        test_total_days = test_strategy_states.size
-        for i in range(1, test_total_days):
-            state = test_strategy_states[i - 1, 0]
+        for days in range(1, test_states.size):
+            state = position * 1000 + test_states[days - 1, 0]
             action = self.learner.querysetstate(state)
-            if p == 0 and action ==1:
+            if position == 0 and action == 1:
 
-                trades.values[i, :] = -1000
-                p = 1
-            elif p ==0 and action == 2:
-                trades.values[i, :] = 1000
-                p = 2
+                trades.values[days, :] = -1000
+                position = 1
+            elif position == 0 and action == 2:
+                trades.values[days, :] = 1000
+                position = 2
 
-            elif p == 1 and action == 2:
-                trades.values[i, :] = 2000
-                p = 2
 
-            elif p == 2 and action == 1:
-                trades.values[i, :] = -2000
-                p = 1
+            elif position == 1 and action == 2:
+                trades.values[days, :] = 2000
+                position = 2
 
+            elif position == 2 and action == 1:
+                trades.values[days, :] = -2000
+                position = 1
 
         if self.verbose: print type(trades)  # it better be a DataFrame!
         if self.verbose: print trades
